4be671fdadadcbbd84eaa9e16f8a8ffa138b944d
Ggskrn <148623788+Ggskrn@users.noreply.github.com>
2024-10-08 07:54:11 +0700

    Reservasi.java

---
+++ Reservasi.java
@@ -0,0 +1,22 @@
+//Gagas Cahya Kamajengan
+//3F Informatika
+//2310631170018
+
+//UTS PRAKTIKUM PBO
+
+//2. Tipe Data, Parameter, Konstruktor dan Interface
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Hotel;
+
+/**
+ *
+ * @author USER
+ */
+public interface Reservasi {
+    void inginreservasi();
+    void cancelReservasi();
+}
+   
